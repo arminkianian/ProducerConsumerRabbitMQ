b86b84f913b9738f095c1ef8a21d3e6236849433
Armin <arminkia.dev@gmail.com>
2020-08-10 19:05:26 +0430

    Add project files.

---
+++ Producer/Sender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var message = "My name is Armin";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+
+                Console.WriteLine("Sent message {0}...", message);
+            }
+
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+        }
+    }
+}
